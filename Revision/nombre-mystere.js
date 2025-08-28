c51f532f7b5dbe538b03c9d994012a072a48c695
jbeaudet-cstj <jbeaudet@cstj.qc.ca>
2025-08-28 15:14:00 -0400

    Fin nombre mystère

--- Revision/nombre-mystere.js
+++ Revision/nombre-mystere.js
@@ -14,17 +14,34 @@ const resultat = document.getElementById("resultat");
 // L'essai courant
 const essaiCourant = document.getElementById("essai");
 
+essaiCourant.addEventListener("keypress", (event) => {
+   if (event.key === "Enter") {
+      verifier();
+   }
+})
+
 function verifier() {
    ++nbTentatives;
    tentatives.innerHTML = "" + nbTentatives + " tentative" + (nbTentatives > 1 ? "s" : "");
 
    if (essaiCourant.value > nombreMystere) {
       resultat.innerHTML = "Trop haut";
+      resultat.className = "tropHaut";
    }
    else if (essaiCourant.value < nombreMystere) {
       resultat.innerHTML = "Trop bas";
+      resultat.className = "tropBas";
    }
    else {
       resultat.innerHTML = "Succès";
+      resultat.className = "succes";
    }
 }
+
+// TODO:
+//  - Valider l'essai entre 1 et 100. Message d'erreur sinon, et n'augmente pas les tentatives
+//  - Lorsque nombre trouvé:
+//     - Enter ne fait rien
+//     - Le bouton indique "Rejouer"
+//     - Si bouton cliqué, recommence un nouveau jeu
+//  - Maximum 10 tentatives
